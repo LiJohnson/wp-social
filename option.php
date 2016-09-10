d4cf8ed01c512c9fe986c1260905f4293212d3a0
LiJohnson <598420668@163.com>
2016-09-10 10:38:23 +0800

    sync weibo

---
+++ option.php
@@ -0,0 +1,49 @@
+<?php
+	require_once __DIR__ . '/MyPHP/lib/saetv2.ex.class.php';
+
+	if( isset($_POST[SOCIAL_KEY]) ){
+		update_option( SOCIAL_KEY , $_POST);
+	}
+	$option = get_option(SOCIAL_KEY);
+
+	$wbOauth = new SaeTOAuthV2 ( $option['wb_app_key'], $option['wb_app_secret'] );
+	$wbOauthUrl =   menu_page_url(SOCIAL_MENU_SLUG,false) . '&wb_oauth=true' ;
+
+	if( isset($_GET['wb_oauth']) && isset( $_GET['code'] ) ){
+		$option['wb_token'] = $wbOauth->getAccessToken ( 'code', ['code' => $_GET['code'] , 'redirect_uri' => $wbOauthUrl ] );
+		update_option(SOCIAL_KEY,$option);
+		echo '<script>location.href="'. menu_page_url(SOCIAL_MENU_SLUG,false) .'";</script>';
+		exit();
+	}
+	
+	$client = new SaeTClientV2( $option['wb_app_key']  , $option['wb_app_secret'] , $option['wb_token']['access_token']);
+	$client->set_debug(true);
+//	var_dump($client);
+	$res = $client->update('abs' . time());
+	var_dump($res);
+
+?>
+
+<form action="" method="post">
+	<table class="form-table">
+		<tr class="alternate" >
+			<td rowspan="2">微博</td>
+			<th><lable for="wb-app-key">app key</lable></th>
+			<td><input id="wb-app-key" type="text"  class="regular-text" name="wb_app_key" value="<?=$option['wb_app_key']?>"></td>
+			<td rowspan="2" > 
+				<?php wp_dropdown_categories( ['name' => 'wb_cat' , 'selected' => $option['wb_cat']] ); ?> 
+				<a href="<?=$wbOauth->getAuthorizeURL($wbOauthUrl)?>" >aouth</a>
+				<?=$option['wb_token']['access_token']?>
+			</td>
+		</tr>
+		<tr class="alternate" >
+			<th>
+				<lable for="wb-app-secret">app secret</lable>
+			</th>
+			<td><input type="text" id="wb-app-secret" class="regular-text" name="wb_app_secret" value="<?=$option['wb_app_secret']?>"></td>
+		</tr>
+		<tr>
+			<td colspan="4"><input type="submit" name="social" value="update" class="button button-primary"></td>
+		</tr>
+	</table>
+</form>
---
+++ wp_social.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: WP-SOCIAL
+Version: 0.0.1
+Plugin URI:https://github.com/LiJohnson/wp-social
+Description: connect to social app
+Author: LiJohnson
+Author URI: http://lcs.io/
+*/
+
+define('SOCIAL_MENU_SLUG', 'WP-SOCIAL');
+define('SOCIAL_KEY', 'social');
+
+add_action('admin_menu', function(){
+	add_options_page('social','social',1,SOCIAL_MENU_SLUG,function(){
+		include __DIR__ . '/option.php';
+	});
+});
+
+
+add_action( 'publish_post', function($id ){
+	$option = get_option(SOCIAL_KEY);
+
+	if( !has_category( $option['wb_cat'] , $post ) )return;
+
+	$post = strip_tags( get_post_field('post_content' , $id ));
+	$images = get_attached_media('image',$id);
+
+	$url = false;
+	if( $images ){
+		$url = array_shift($images);
+		$url = $url->guid;
+	}
+
+	if( isset($images[0]) )$url = $images[0];
+//	var_dump([$post,$url]);exit();
+
+	require_once  __DIR__ . '/MyPHP/lib/saetv2.ex.class.php';
+
+	$client = new SaeTClientV2( $option['wb_app_key']  , $option['wb_app_secret'] , $option['wb_token']['access_token']);
+
+	if( !$url ){
+		$client->update( $post );
+	}else{
+		$client->upload( $post , $url);
+	}
+
+});
+
